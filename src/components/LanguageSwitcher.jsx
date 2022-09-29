cb584bc79e3de1db82707902a9bb84303d4ce1de
uniqueprogrammer <uniqueprogrammer@hotmail.com>
2022-09-29 16:34:43 +0300

    unknown proplem solved

---
+++ src/components/LanguageSwitcher.jsx
@@ -0,0 +1,32 @@
+import {Link} from "react-router-dom";
+import {ReactComponent as DownloadIcon} from '../assets/imgs/download.svg';
+//import '../assets/css/LanguageSwitcher.css';
+
+export default function LanguageSwitcher(props) {
+	const languages=[
+		{"name": "it", "title": "Italiana"},
+		{"name": "en", "title": "English"},
+		{"name": "fr", "title": "française"}
+	];
+
+	for(let i=0; i<languages.length; i++)
+		languages[i].flag=require("../assets/imgs/flag-"+languages[i].name+".gif");
+
+	return (
+		<div className="switcher">
+		{
+			languages.map(language=>(
+				<div key={language.name} className="switcher-container">
+					<Link className="switcher-container__link" to={props.pageLink.replace("-it", language.name)}>
+						<img className="switcher-container__flag" src={language.flag} alt={language.title+" flag"}/>
+					</Link>
+					<Link className="switcher-container__link" to={props.downloadLink.replace("-it", language.name)}>
+						<DownloadIcon />
+					</Link>
+
+				</div>
+			))
+		}
+		</div>
+	);
+}
