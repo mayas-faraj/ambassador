e0d4fb4ae120c101eb512bd9a8c855f9ed404ec9
Mayas Faraj <uniqueprogrammer@hotmail.com>
2022-11-14 16:41:37 +0300

    add footer urls

--- components/footer.jsx
+++ components/footer.jsx
@@ -6,26 +6,26 @@ import style from "/style/footer.module.scss";
 
 export default function Footer() {
 	const icons=[
-		{"image": "gran-teatro.png", "alt": "gran teatro logo", "title": "Gran teatro\nla fenice"},
-		{"image": "associazione-nazionale.png", "alt": "associazione nazionale logo", "title": "Associazione Nazionale\nCarabinieri"},
-		{"image": "presidenza-della.png", "alt": "presidenza della logo", "title": "Presidenza della \nrepubblica Italiana"},
-		{"image": "assocazione-nazionale-diplomatici.png", "alt": "assocazione nazionale diplomatici logo", "title": "Assocazione\nNazionale Diplomatici\nar.- constantino nigra"},
-		{"image": "l-accademia-etrusca.png", "alt": "l-accademia etrusca logo", "title": "L’ACCADEMIA \nETRUSCA DI CORTONA"},
-		{"image": "istituto-euro-mediterraneo.png", "alt": "istituto euro mediterraneo logo", "title": "Istituto \nEuro-mediterraneo\ne Per i paesi arabi"},
-		{"image": "ambasciata-tripoli.png", "alt": "ambasciata tripoli logo", "title": ""},
-		{"image": "ambasciata-cairo.png", "alt": "ambasciata cairo logo", "title": ""},
-		{"image": "ambasciata-teheran.png", "alt": "ambasciata teheran logo", "title": ""},
-		{"image": "ambasciata-khartoum.png", "alt": "ambasciata khartoum logo", "title": ""},
-		{"image": "ambasciata-dhaka.png", "alt": "ambasciata dhaka logo", "title": ""},
-		{"image": "ambasciata-washington-dc.png", "alt": "ambasciata washington logo", "title": ""},
-		{"image": "sapienza.png", "alt": "sapienze logo", "title": "La sapienza\nuniversità di roma"},
-		{"image": "luiss.png", "alt": "luiss logo", "title": "Luiss Università\nguido carli"},
-		{"image": ""},
-		{"image": "harvard-university.png", "alt": "harvard university logo", "title": ""},
-		{"image": "associazione-pergine.png", "alt": "associazione pergine logo", "title": "Associazione \nAmici della storia"},
-		{"image": "studi-diplomatici.png", "alt": "studi diplomatici logo", "title": "Circolo studi\ndiplomatici"},
-		{"image": "cambridge-university.png", "alt": "cambridge university logo", "title": ""},
-		{"image": ""},
+		{"image": "gran-teatro.png", "alt": "gran teatro logo", "title": "Gran teatro\nla fenice", "link": "https://www.teatrolafenice.it/"},
+		{"image": "associazione-nazionale.png", "alt": "associazione nazionale logo", "title": "Associazione Nazionale\nCarabinieri", "link": "https://assocarabinieri.it/"},
+		{"image": "presidenza-della.png", "alt": "presidenza della logo", "title": "Presidenza della \nrepubblica Italiana", "link": "https://www.quirinale.it/onorificenze/ricerca"},
+		{"image": "assocazione-nazionale-diplomatici.png", "alt": "assocazione nazionale diplomatici logo", "title": "Assocazione\nNazionale Diplomatici\nar.- constantino nigra", "link": "http://www.assdiplar.it/default.php"},
+		{"image": "l-accademia-etrusca.png", "alt": "l-accademia etrusca logo", "title": "L’ACCADEMIA \nETRUSCA DI CORTONA", "link": "https://www.accademia-etrusca.org/"},
+		{"image": "istituto-euro-mediterraneo.png", "alt": "istituto euro mediterraneo logo", "title": "Istituto \nEuro-mediterraneo\ne Per i paesi arabi", "link": "http://www.istitutoperilmondoarabo.it"},
+		{"image": "ambasciata-tripoli.png", "alt": "ambasciata tripoli logo", "title": "", "link": "https://ambtripoli.esteri.it/ambasciata_tripoli/it/"},
+		{"image": "ambasciata-cairo.png", "alt": "ambasciata cairo logo", "title": "", "link": "https://ambilcairo.esteri.it/ambasciata_ilcairo/it/ambasciata/news/dall-ambasciata/2010/06/l-ambasciatore-d-italia-al-cairo-s-e-claudio-pacifico-incontra-il-segretario-generale-della-lega-ar.html"},
+		{"image": "ambasciata-teheran.png", "alt": "ambasciata teheran logo", "title": "", "link": "https://ambteheran.esteri.it/ambasciata_teheran/it/"},
+		{"image": "ambasciata-khartoum.png", "alt": "ambasciata khartoum logo", "title": "", "link": "https://ambkhartoum.esteri.it/ambasciata_khartoum/it/"},
+		{"image": "ambasciata-dhaka.png", "alt": "ambasciata dhaka logo", "title": "", "link": "https://ambdhaka.esteri.it/ambasciata_dhaka/it/ambasciata/"},
+		{"image": "ambasciata-washington-dc.png", "alt": "ambasciata washington logo", "title": "", "link": "https://ambwashingtondc.esteri.it/ambasciata_washington/it"},
+		{"image": "sapienza.png", "alt": "sapienze logo", "title": "La sapienza\nuniversità di roma", "link": "https://www.uniroma1.it/it/pagina-strutturale/home"},
+		{"image": "luiss.png", "alt": "luiss logo", "title": "Luiss Università\nguido carli", "link": "https://www.luiss.it/"},
+		{"image": "", "link": ""},
+		{"image": "harvard-university.png", "alt": "harvard university logo", "title": "", "link": "https://www.harvard.edu/"},
+		{"image": "associazione-pergine.png", "alt": "associazione pergine logo", "title": "Associazione \nAmici della storia", "link": "https://www.amicidellastoriapergine.it/"},
+		{"image": "studi-diplomatici.png", "alt": "studi diplomatici logo", "title": "Circolo studi\ndiplomatici", "link": "http://www.studidiplomatici.it/il-circolo/"},
+		{"image": "cambridge-university.png", "alt": "cambridge university logo", "title": "", "link": "https://www.cam.ac.uk/"},
+		{"image": "", "link": ""},
 	];
 
 	const [windowWidth, setWindowWidth]=React.useState(-1);
@@ -45,7 +45,7 @@ export default function Footer() {
 						{
 							icons.filter((icon, index)=>index<6).map((icon, index)=>(
 								<div key={icon.image} className={style["footer-icons__item"]+(icon.title.split("\n").length==2?" "+style["footer-icons__item--small-caption"]:"")}>
-									<a target="_blank" href="#" className={style["footer-icons__link"]}>
+									<a target="_blank" href={icon.link} className={style["footer-icons__link"]}>
 										<img src={"/assets/imgs/footer/"+icon.image} alt={icon.alt}/>
 										<div className={style["footer-icons__title"]}>{icon.title}</div>
 									</a>
@@ -57,7 +57,7 @@ export default function Footer() {
 						{
 							icons.filter((icon, index)=>index>=6 && index<12).map((icon, index)=>(
 								<div key={icon.image} className={style["footer-icons__item"]}>
-									<a target="_blank" href="#" className={style["footer-icons__link"]}>
+									<a target="_blank" href={icon.link} className={style["footer-icons__link"]}>
 										<img src={"/assets/imgs/footer/"+icon.image} alt={icon.alt}/>
 										<div className={style["footer-icons__title"]}>{icon.title}</div>
 									</a>
@@ -70,7 +70,7 @@ export default function Footer() {
 							{
 								icons.filter((icon, index)=>index>=12 && index<16).map((icon, index)=>(
 									<div key={icon.image} className={style["footer-icons__item"]+(icon.image?"":" "+style["footer-icons__item--transparent"])}>
-										<a target="_blank" href="#" className={style["footer-icons__link"]}>
+										<a target="_blank" href={icon.link} className={style["footer-icons__link"]}>
 											<img src={icon.image?"/assets/imgs/footer/"+icon.image:"/assets/imgs/transparent.png"} alt={icon.alt}/>
 											<div className={style["footer-icons__title"]}>{icon.title}</div>
 										</a>
@@ -97,7 +97,7 @@ export default function Footer() {
 							{
 								icons.filter((icon, index)=>index>=16 && index<20).map((icon, index)=>(
 									<div key={icon.image} className={style["footer-icons__item"]+(icon.image?"":" "+style["footer-icons__item--transparent"])}>
-										<a target="_blank" href="#" className={style["footer-icons__link"]}>
+										<a target="_blank" href={icon.link} className={style["footer-icons__link"]}>
 											<img src={icon.image?"/assets/imgs/footer/"+icon.image:"/assets/imgs/transparent.png"} alt={icon.alt}/>
 											<div className={style["footer-icons__title"]}>{icon.title}</div>
 										</a>
@@ -113,7 +113,7 @@ export default function Footer() {
 						<Swiper className={style["footer-swiper"]} modules={[Navigation]} spaceBetween={0} loop={true} grabCursor={true} slidesPerView={2} navigation>
 						{icons.filter((icon, index)=>index<6).map(icon=>icon.image && (
 							<SwiperSlide key={icon.image} className={style["footer-icons__slide"]}>
-								<a target="_blank" href="#" className={style["footer-icons__link"]}>
+								<a target="_blank" href={icon.link} className={style["footer-icons__link"]}>
 									<img src={"/assets/imgs/footer/"+icon.image} alt={icon.alt}/>
 									<div className={style["footer-icons__title"]}>{icon.title}</div>
 								</a>
@@ -124,7 +124,7 @@ export default function Footer() {
 						<Swiper className={style["footer-swiper"]} modules={[Navigation]} spaceBetween={0} loop={true} grabCursor={true} slidesPerView={2} navigation>
 						{icons.filter((icon, index)=>(index>=6 && index<12)).map(icon=>icon.image && (
 							<SwiperSlide key={icon.image} className={style["footer-icons__slide"]}>
-								<a target="_blank" href="#" className={style["footer-icons__link"]}>
+								<a target="_blank" href={icon.link} className={style["footer-icons__link"]}>
 									<img src={"/assets/imgs/footer/"+icon.image} alt={icon.alt}/>
 									<div className={style["footer-icons__title"]}>{icon.title}</div>
 								</a>
@@ -135,7 +135,7 @@ export default function Footer() {
 						<Swiper className={style["footer-swiper"]} modules={[Navigation]} spaceBetween={0} loop={true} grabCursor={true} slidesPerView={2} navigation>
 						{icons.filter((icon, index)=>index>=12).map(icon=>icon.image && (
 							<SwiperSlide key={icon.image} className={style["footer-icons__slide"]}>
-								<a target="_blank" href="#" className={style["footer-icons__link"]}>
+								<a target="_blank" href={icon.link} className={style["footer-icons__link"]}>
 									<img src={"/assets/imgs/footer/"+icon.image} alt={icon.alt}/>
 									<div className={style["footer-icons__title"]}>{icon.title}</div>
 								</a>
